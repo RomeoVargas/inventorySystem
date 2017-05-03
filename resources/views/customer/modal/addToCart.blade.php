4f78cf5e56e830afb98984264a55ef807c3de64c
RomeoVargas <romeovillegasvargas@gmail.com>
2017-05-03 20:19:23 +0800

    Fixed minor bugs

--- resources/views/customer/modal/addToCart.blade.php
+++ resources/views/customer/modal/addToCart.blade.php
@@ -5,7 +5,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">{{ $cartId ? 'Edit' : 'Add' }} Cart Item</h4>
+                <h4 class="modal-title">{{ $user ? (($cartId ? 'Edit' : 'Add') . ' Cart Item' ) : $name }}</h4>
             </div>
             <div class="modal-body">
                 <form method="POST" action="{{ url('cart/save') }}" class="form-horizontal row">
